ba79cca59a4db587e5fddeed3250d1e294a5f026
smitinit <patelsmit090305@gmail.com>
2026-01-12 11:01:06 +0530

    implemented context.consumer

--- src/components/TestComp2.tsx
+++ src/components/TestComp2.tsx
@@ -1,29 +1,33 @@
+import { TestContext } from "@/context/TestContext";
 import { Input } from "./ui/input";
 
 import { Label } from "./ui/label";
-import { useTest } from "@/hooks/test-context-hook";
 
 export function Test2() {
-  const { name, age, setValues } = useTest();
-
   return (
-    <>
-      <Label>Name input, belongs to test 2</Label>
-      <Input
-        type="text"
-        placeholder="
-                This input belongs to App component, Enter name..."
-        value={name}
-        onChange={(e) =>
-          setValues((pV) => {
-            return {
-              ...pV,
-              name: e.target.value,
-            };
-          })
-        }
-      />
-      This is in the test2, AGE:{age}
-    </>
+    <TestContext.Consumer>
+      {({ name, age, setValues }) => {
+        return (
+          <>
+            <Label>Name input, belongs to test 2</Label>
+            <Input
+              type="text"
+              placeholder="
+                  This input belongs to App component, Enter name..."
+              value={name}
+              onChange={(e) =>
+                setValues((pV) => {
+                  return {
+                    ...pV,
+                    name: e.target.value,
+                  };
+                })
+              }
+            />
+            This is in the test2, AGE:{age}
+          </>
+        );
+      }}
+    </TestContext.Consumer>
   );
 }
